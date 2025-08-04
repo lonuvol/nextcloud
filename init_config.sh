0d89dc28d6e9b2256c54fefdf21798164c2767ae
rberenguermayench <69071969+rberenguermayench@users.noreply.github.com>
2025-08-04 15:16:27 +0200

    Update init_config.sh

--- init_config.sh
+++ init_config.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 set -e
 
+if [ ! -f /var/www/html/occ ]; then
+  echo "❌ No s'ha trobat /var/www/html/occ. Sortint..."
+  exit 1
+fi
+
+echo "⏳ Esperant que Nextcloud estigui instal·lat i preparat..."
+until php /var/www/html/occ status | grep -q "installed"; do
+  echo "⌛ Nextcloud no està llest. Reintentant en 5s..."
+  sleep 5
+done
+
+echo "✅ Nextcloud està preparat. Iniciant configuració..."
 echo "⏳ Esperant que Nextcloud estigui instal·lat i preparat..."
 
-until sudo -u abc php /config/www/nextcloud/occ status | grep -q "installed"; do
+until sudo -u abc php /var/www/html/occ status | grep -q "installed"; do
   echo "⌛ Nextcloud no està llest. Reintentant en 5s..."
   sleep 5
 done
